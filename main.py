2751682ec40ffe81df051b8fabc89faa69bca0b7
atrox3d <atrox3d@gmail.com>
2021-02-23 06:00:34 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def get_iss_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    latitude = data["iss_position"]["latitude"]
+    longitude = data["iss_position"]["longitude"]
+
+    iss_position = (latitude, longitude)
+    return iss_position
+
+
