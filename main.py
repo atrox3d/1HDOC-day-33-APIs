c8aa9afcd94cc90e31173a028bcf4fec54696509
atrox3d <atrox3d@gmail.com>
2021-02-23 07:47:45 +0100

    detect if ISS is above and is night at the moment

--- main.py
+++ main.py
@@ -15,8 +15,39 @@ def get_iss_position():
     latitude = data["iss_position"]["latitude"]
     longitude = data["iss_position"]["longitude"]
 
-    iss_position = (latitude, longitude)
-    return iss_position
+    iss_position_tuple = (float(latitude), float(longitude))
+    iss_position_dict = {
+        "latitude": float(latitude),
+        "longitude": float(longitude)
+    }
+    return iss_position_dict
+
+
+def is_iss_above(latitude, longitude):
+    iss_position = get_iss_position()
+    current_position = {
+        "latitude": float(latitude),
+        "longitude": float(longitude)
+    }
+    print("iss_position    : ", iss_position)
+    print("current_position: ", current_position)
+
+    print("(current latitude - 5.0) <= iss latitude <=  (current latitude + 5.0) : ", end="")
+    if (current_position["latitude"] - 5.0) <= iss_position["latitude"] <= (current_position["latitude"] + 5.0):
+        print(True)
+        print("(current longitude - 5.0) <= iss longitude <=  (current longitude + 5.0) : ", end="")
+        if (current_position["longitude"] - 5.0) <= iss_position["longitude"] <= (current_position["longitude"] + 5.0):
+            print(True)
+            print("iss is above: ", True)
+            return True
+        else:
+            print(False)
+            print("iss is above: ", False)
+            return False
+    else:
+        print(False)
+        print("iss is above: ", False)
+        return False
 
 
 def get_sunrise_sunset(latitude, longitude, formatted=False):
@@ -28,9 +59,9 @@ def get_sunrise_sunset(latitude, longitude, formatted=False):
     :return: tuple(sunrise, sunset)
     """
     params = {
-        "lat":  latitude,
-        "lng":  longitude,
-        "formatted":    1 if formatted else 0,
+        "lat": latitude,
+        "lng": longitude,
+        "formatted": 1 if formatted else 0,
     }
     response = requests.get("https://api.sunrise-sunset.org/json", params=params)
     # print(response.url)
@@ -41,17 +72,36 @@ def get_sunrise_sunset(latitude, longitude, formatted=False):
     return sunrise, sunset
 
 
-iss_position = get_iss_position()
+def is_night(latitude, longitude):
+    sunrise, sunset = get_sunrise_sunset(latitude, longitude)
+    sunrise_hour = int(sunrise.split("T")[1].split(":")[0])
+    sunset_hour = int(sunset.split("T")[1].split(":")[0])
+
+    current_hour = dt.datetime.now().hour
+    print(type(current_hour))
+    print("sunrise hour    : ", sunrise_hour)
+    print("sunset hour     : ", sunset_hour)
+    print("current hour    : ", current_hour)
+
+    print("sunrise_hour >= current_hour >= sunset_hour: ", end="")
+    if sunrise_hour >= current_hour >= sunset_hour:
+        print(True)
+        print("is night: ", True)
+        return True
+    else:
+        print(True)
+        print("is night: ", False)
+        return False
+
 
 TURIN_LATITUDE = 45.068371
 TURIN_LONGITUDE = 7.683070
-sunrise, sunset = get_sunrise_sunset(TURIN_LATITUDE, TURIN_LONGITUDE)
-sunrise_hour = sunrise.split("T")[1].split(":")[0]
-sunset_hour = sunset.split("T")[1].split(":")[0]
 
-print("ISS current position              : ", iss_position)
-print("Sunrise/Sunset at current location: ", sunrise, sunset )
-print("Sunrise hour 24h                  : ", sunrise_hour)
-print("Sunset hour 24h                   : ", sunset_hour)
-print("current time                      : ", dt.datetime.now())
-print("current hour                      : ", dt.datetime.now().hour)
+iss_above = is_iss_above(latitude=TURIN_LATITUDE, longitude=TURIN_LONGITUDE)
+is_dark = is_night(TURIN_LATITUDE, TURIN_LONGITUDE)
+
+if iss_above and is_dark:
+    # send mail
+    print("you can see the ISS now")
+else:
+    print("you cannot see the ISS now")
